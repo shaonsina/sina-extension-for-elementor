f062cccc223443800ecaa805d99891635cc2ed43
shaonsina <shaon.sina@gmail.com>
2019-05-10 15:49:58 +0600

    sina table started

--- widgets/basic/sina-table.php
+++ widgets/basic/sina-table.php
@@ -11,6 +11,7 @@ use Elementor\Group_Control_Typography;
 use Elementor\Group_Control_Text_Shadow;
 use Elementor\Group_Control_Border;
 use Elementor\Group_Control_Box_Shadow;
+use Elementor\Repeater;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -87,18 +88,116 @@ class Sina_Table_Widget extends Widget_Base {
 	 * @since 2.1.0
 	 */
 	protected function _register_controls() {
-		// Start Table Content
+		// Start Table Header
 		// ====================
 		$this->start_controls_section(
-			'table_content',
+			'table_header',
 			[
-				'label' => __( 'Table Content', 'sina-ext' ),
+				'label' => __( 'Table Header', 'sina-ext' ),
 				'tab' => Controls_Manager::TAB_CONTENT,
 			]
 		);
 
+		$thead = new Repeater();
+
+		$thead->add_control(
+			'header_text',
+			[
+				'label' => __( 'Header Text', 'sina-ext' ),
+				'label_block' => true,
+				'type' => Controls_Manager::TEXT,
+				'placeholder' => __('Enter Text', 'sina-ext'),
+				'default' => __('WordPress', 'sina-ext'),
+			]
+		);
+		$this->add_control(
+			'header_content',
+			[
+				'label' => __('Add Item', 'sina-ext'),
+				'type' => Controls_Manager::REPEATER,
+				'fields' => $thead->get_controls(),
+				'prevent_empty' => false,
+				'default' => [
+					[
+						'header_text' => __('WordPress', 'sina-ext'),
+					],
+				],
+				'title_field' => '{{{ header_text }}}',
+			]
+		);
+
 		$this->end_controls_section();
-		// End Table Content
+		// End Table Header
+		// ==================
+
+		// Start Table Body
+		// ====================
+		$this->start_controls_section(
+			'table_body',
+			[
+				'label' => __( 'Table Body', 'sina-ext' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$tbody = new Repeater();
+
+		$tbody->add_control(
+			'content_type',
+			[
+				'label' => __( 'Content Type', 'sina-ext' ),
+				'type' => Controls_Manager::SELECT,
+				'options' => [
+					'row' => __( 'Row', 'sina-ext' ),
+					'cell' => __( 'Cell', 'sina-ext' ),
+					'head' => __( 'Head', 'sina-ext' ),
+				],
+				'default' => 'row',
+			]
+		);
+
+		$tbody->add_control(
+			'row_span',
+			[
+				'label' => __( 'Row Span', 'sina-ext' ),
+				'type' => Controls_Manager::NUMBER,
+				'default' => 1,
+				'min' => 1,
+				'condition' => [
+					'content_type' => ['cell', 'head'],
+				],
+			]
+		);
+		$tbody->add_control(
+			'col_span',
+			[
+				'label' => __( 'Column Span', 'sina-ext' ),
+				'type' => Controls_Manager::NUMBER,
+				'default' => 1,
+				'min' => 1,
+				'condition' => [
+					'content_type' => ['cell', 'head'],
+				],
+			]
+		);
+		$this->add_control(
+			'body_content',
+			[
+				'label' => __('Add Item', 'sina-ext'),
+				'type' => Controls_Manager::REPEATER,
+				'fields' => $tbody->get_controls(),
+				'default' => [
+					[
+						'row_span' => __('1', 'sina-ext'),
+						'col_span' => __('1', 'sina-ext'),
+					],
+				],
+				'title_field' => '{{{ content_type }}}',
+			]
+		);
+
+		$this->end_controls_section();
+		// End Table Body
 		// ==================
 	}
 
@@ -107,6 +206,33 @@ class Sina_Table_Widget extends Widget_Base {
 		$data = $this->get_settings_for_display();
 		?>
 		<div class="sina-table">
+			<table>
+				<caption>List of users</caption>
+				<thead>
+					<tr>
+						<th>fine</th>
+						<th>fine</th>
+						<th>fine</th>
+						<th>fine</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>nice</th>
+						<td>nice</td>
+						<td>nice</td>
+						<td>nice</td>
+						<td>nice</td>
+					</tr>
+					<tr>
+						<th>nice</th>
+						<td>nice</td>
+						<td>nice</td>
+						<td>nice</td>
+						<td>nice</td>
+					</tr>
+				</tbody>
+			</table>
 		</div><!-- .sina-table -->
 		<?php
 	}
