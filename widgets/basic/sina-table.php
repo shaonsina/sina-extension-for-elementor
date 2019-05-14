0eb57790837b3d74a7ba5da833727ce75a23200d
shaonsina <shaon.sina@gmail.com>
2019-05-14 21:11:05 +0600

    Table content particular style generated

--- widgets/basic/sina-table.php
+++ widgets/basic/sina-table.php
@@ -160,6 +160,69 @@ class Sina_Table_Widget extends Widget_Base {
 				],
 			]
 		);
+
+		$thead->start_controls_tabs( 'header_style_tabs' );
+
+		$thead->start_controls_tab(
+			'header_normal',
+			[
+				'label' => __( 'Normal', 'sina-ext' ),
+			]
+		);
+
+		$thead->add_control(
+			'header_color',
+			[
+				'label' => __( 'Text Color', 'sina-ext' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sina-table table thead tr {{CURRENT_ITEM}}' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$thead->add_group_control(
+			Group_Control_Background::get_type(),
+			[
+				'name' => 'header_background',
+				'label' => __( 'Background', 'sina-ext' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .sina-table table thead tr {{CURRENT_ITEM}}',
+			]
+		);
+
+		$thead->end_controls_tab();
+
+		$thead->start_controls_tab(
+			'header_hover',
+			[
+				'label' => __( 'Hover', 'sina-ext' ),
+			]
+		);
+
+		$thead->add_control(
+			'header_hover_color',
+			[
+				'label' => __( 'Text Color', 'sina-ext' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sina-table table thead tr {{CURRENT_ITEM}}:hover' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$thead->add_group_control(
+			Group_Control_Background::get_type(),
+			[
+				'name' => 'header_hover_background',
+				'label' => __( 'Background', 'sina-ext' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .sina-table table thead tr {{CURRENT_ITEM}}:hover',
+			]
+		);
+
+		$thead->end_controls_tab();
+
+		$thead->end_controls_tabs();
+
 		$this->add_control(
 			'header_content',
 			[
@@ -214,7 +277,6 @@ class Sina_Table_Widget extends Widget_Base {
 				'default' => 'row',
 			]
 		);
-
 		$tbody->add_control(
 			'row_span',
 			[
@@ -250,6 +312,69 @@ class Sina_Table_Widget extends Widget_Base {
 				],
 			]
 		);
+
+		$tbody->start_controls_tabs( 'content_style_tabs' );
+
+		$tbody->start_controls_tab(
+			'content_normal',
+			[
+				'label' => __( 'Normal', 'sina-ext' ),
+			]
+		);
+
+		$tbody->add_control(
+			'content_color',
+			[
+				'label' => __( 'Text Color', 'sina-ext' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} {{CURRENT_ITEM}}' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$tbody->add_group_control(
+			Group_Control_Background::get_type(),
+			[
+				'name' => 'content_background',
+				'label' => __( 'Background', 'sina-ext' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} {{CURRENT_ITEM}}',
+			]
+		);
+
+		$tbody->end_controls_tab();
+
+		$tbody->start_controls_tab(
+			'content_hover',
+			[
+				'label' => __( 'Hover', 'sina-ext' ),
+			]
+		);
+
+		$tbody->add_control(
+			'content_hover_color',
+			[
+				'label' => __( 'Text Color', 'sina-ext' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} {{CURRENT_ITEM}}:hover' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$tbody->add_group_control(
+			Group_Control_Background::get_type(),
+			[
+				'name' => 'content_hover_background',
+				'label' => __( 'Background', 'sina-ext' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} {{CURRENT_ITEM}}:hover',
+			]
+		);
+
+		$tbody->end_controls_tab();
+
+		$tbody->end_controls_tabs();
+
 		$this->add_control(
 			'body_content',
 			[
@@ -283,33 +408,6 @@ class Sina_Table_Widget extends Widget_Base {
 						'row_span' => 1,
 						'col_span' => 1,
 						'cell_content' => '28',
-					],
-					[
-						'content_type' => 'row',
-					],
-					[
-						'content_type' => 'head',
-						'row_span' => 1,
-						'col_span' => 1,
-						'cell_content' => '2020',
-					],
-					[
-						'content_type' => 'cell',
-						'row_span' => 1,
-						'col_span' => 1,
-						'cell_content' => 'Steve',
-					],
-					[
-						'content_type' => 'cell',
-						'row_span' => 1,
-						'col_span' => 1,
-						'cell_content' => 'Rhodes',
-					],
-					[
-						'content_type' => 'cell',
-						'row_span' => 1,
-						'col_span' => 1,
-						'cell_content' => '32',
 					],
 				],
 				'title_field' => '{{{ content_type }}}',
@@ -363,6 +461,112 @@ class Sina_Table_Widget extends Widget_Base {
 				'selector' => '{{WRAPPER}} .sina-table table thead th',
 			]
 		);
+
+		$this->start_controls_tabs( 'header_tabs' );
+
+		$this->start_controls_tab(
+			'header_normal',
+			[
+				'label' => __( 'Normal', 'sina-ext' ),
+			]
+		);
+
+		$this->add_control(
+			'header_color',
+			[
+				'label' => __( 'Text Color', 'sina-ext' ),
+				'type' => Controls_Manager::COLOR,
+				'default' => '#eee',
+				'selectors' => [
+					'{{WRAPPER}} .sina-table table thead tr' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$this->add_group_control(
+			Group_Control_Background::get_type(),
+			[
+				'name' => 'header_background',
+				'label' => __( 'Background', 'sina-ext' ),
+				'types' => [ 'classic', 'gradient' ],
+				'fields_options' => [
+					'background' => [ 
+						'default' =>'classic', 
+					],
+					'color' => [
+						'default' => '#1085e4',
+					],
+				],
+				'selector' => '{{WRAPPER}} .sina-table table thead tr',
+			]
+		);
+		$this->add_group_control(
+			Group_Control_Border::get_type(),
+			[
+				'name' => 'header_border',
+				'fields_options' => [
+					'border' => [
+						'default' => 'solid',
+					],
+					'color' => [
+						'default' => '#eee',
+					],
+					'width' => [
+						'default' => [
+							'top' => '1',
+							'right' => '1',
+							'bottom' => '1',
+							'left' => '1',
+							'isLinked' => true,
+						]
+					],
+				],
+				'selector' => '{{WRAPPER}} .sina-table table thead th',
+			]
+		);
+
+		$this->end_controls_tab();
+
+		$this->start_controls_tab(
+			'header_hover',
+			[
+				'label' => __( 'Hover', 'sina-ext' ),
+			]
+		);
+
+		$this->add_control(
+			'header_hover_color',
+			[
+				'label' => __( 'Text Color', 'sina-ext' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sina-table table thead tr:hover' => 'color: {{VALUE}};',
+				],
+			]
+		);
+		$this->add_group_control(
+			Group_Control_Background::get_type(),
+			[
+				'name' => 'header_hover_background',
+				'label' => __( 'Background', 'sina-ext' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .sina-table table thead tr:hover',
+			]
+		);
+		$this->add_control(
+			'header_hover_border',
+			[
+				'label' => __( 'Border Color', 'sina-ext' ),
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sina-table table thead tr:hover th' => 'border-color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->end_controls_tab();
+
+		$this->end_controls_tabs();
+
 		$this->add_responsive_control(
 			'header_padding',
 			[
@@ -376,6 +580,7 @@ class Sina_Table_Widget extends Widget_Base {
 					'left' => '15',
 					'isLinked' => false,
 				],
+				'separator' => 'before',
 				'selectors' => [
 					'{{WRAPPER}} .sina-table table thead th' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
@@ -411,134 +616,6 @@ class Sina_Table_Widget extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'header_cell',
-			[
-				'label' => __( 'Cell Style', 'sina-ext' ),
-				'type' => Controls_Manager::HEADING,
-				'separator' => 'before',
-			]
-		);
-
-		$this->start_controls_tabs( 'header_tabs' );
-
-		$this->start_controls_tab(
-			'header_normal',
-			[
-				'label' => __( 'Normal', 'sina-ext' ),
-			]
-		);
-
-		$this->add_control(
-			'header_color',
-			[
-				'label' => __( 'Text Color', 'sina-ext' ),
-				'type' => Controls_Manager::COLOR,
-				'selectors' => [
-					'{{WRAPPER}} .sina-table table thead th' => 'color: {{VALUE}};',
-				],
-			]
-		);
-		$this->add_group_control(
-			Group_Control_Background::get_type(),
-			[
-				'name' => 'header_background',
-				'label' => __( 'Background', 'sina-ext' ),
-				'types' => [ 'classic', 'gradient' ],
-				'selector' => '{{WRAPPER}} .sina-table table thead th',
-			]
-		);
-		$this->add_group_control(
-			Group_Control_Border::get_type(),
-			[
-				'name' => 'header_border',
-				'fields_options' => [
-					'border' => [
-						'default' => 'solid',
-					],
-					'color' => [
-						'default' => '#ccc',
-					],
-					'width' => [
-						'default' => [
-							'top' => '1',
-							'right' => '1',
-							'bottom' => '1',
-							'left' => '1',
-							'isLinked' => true,
-						]
-					],
-				],
-				'selector' => '{{WRAPPER}} .sina-table table thead th',
-			]
-		);
-
-		$this->end_controls_tab();
-
-		$this->start_controls_tab(
-			'header_hover',
-			[
-				'label' => __( 'Hover', 'sina-ext' ),
-			]
-		);
-
-		$this->add_control(
-			'header_hover_color',
-			[
-				'label' => __( 'Text Color', 'sina-ext' ),
-				'type' => Controls_Manager::COLOR,
-				'default' => '#eee',
-				'selectors' => [
-					'{{WRAPPER}} .sina-table table thead th:hover' => 'color: {{VALUE}};',
-				],
-			]
-		);
-		$this->add_group_control(
-			Group_Control_Background::get_type(),
-			[
-				'name' => 'header_hover_background',
-				'label' => __( 'Background', 'sina-ext' ),
-				'types' => [ 'classic', 'gradient' ],
-				'fields_options' => [
-					'background' => [ 
-						'default' =>'classic', 
-					],
-					'color' => [
-						'default' => '#1085e4',
-					],
-				],
-				'selector' => '{{WRAPPER}} .sina-table table thead th:hover',
-			]
-		);
-		$this->add_group_control(
-			Group_Control_Border::get_type(),
-			[
-				'name' => 'header_hover_border',
-				'fields_options' => [
-					'border' => [
-						'default' => 'solid',
-					],
-					'color' => [
-						'default' => '#1085e4',
-					],
-					'width' => [
-						'default' => [
-							'top' => '1',
-							'right' => '1',
-							'bottom' => '1',
-							'left' => '1',
-							'isLinked' => true,
-						]
-					],
-				],
-				'selector' => '{{WRAPPER}} .sina-table table thead th:hover',
-			]
-		);
-
-		$this->end_controls_tab();
-
-		$this->end_controls_tabs();
-
 		$this->end_controls_section();
 		// End Table Body
 		// ==================
@@ -549,28 +626,30 @@ class Sina_Table_Widget extends Widget_Base {
 		$data = $this->get_settings_for_display();
 
 		$rows = [];
-		$i = 0;
+		$tid = 0;
 		foreach ($data['body_content'] as $key => $content) {
 			if ( 'row' == $content['content_type'] ) {
-				$rows[] = [];
-				$i++;
-			}
-			elseif ( 'head' == $content['content_type'] && isset( $rows[$i-1] ) ) {
-				array_push($rows[$i-1], [
+				$tid = $content['_id'];
+				$rows[ $tid ] = [];
+			} elseif ( 'head' == $content['content_type'] && isset( $rows[ $tid ] ) ) {
+				array_push($rows[ $tid ], [
 					'type' => 'th',
+					'id' => $content['_id'],
 					'row_span' => $content['row_span'],
 					'col_span' => $content['col_span'],
 					'cell_content' => $content['cell_content'],
 				]);
-			} elseif ( 'cell' == $content['content_type'] && isset( $rows[$i-1] ) ) {
-				array_push($rows[$i-1], [
+			} elseif ( 'cell' == $content['content_type'] && isset( $rows[ $tid ] ) ) {
+				array_push($rows[ $tid ], [
 					'type' => 'td',
+					'id' => $content['_id'],
 					'row_span' => $content['row_span'],
 					'col_span' => $content['col_span'],
 					'cell_content' => $content['cell_content'],
 				]);
 			}
 		}
+		// fw_print($rows);
 		// fw_print($data);
 		?>
 		<div class="sina-table">
@@ -580,19 +659,23 @@ class Sina_Table_Widget extends Widget_Base {
 					<thead>
 						<tr>
 							<?php foreach ($data['header_content'] as $content): ?>
-								<th colspan="<?php echo esc_attr( $content['header_col_span'] ); ?>"><?php echo esc_html( $content['header_text'] ); ?></th>
+								<th colspan="<?php echo esc_attr( $content['header_col_span'] ); ?>"
+									class="elementor-repeater-item-<?php echo esc_attr( $content['_id'] ); ?>">
+									<?php echo esc_html( $content['header_text'] ); ?>
+								</th>
 							<?php endforeach; ?>
 						</tr>
 					</thead>
 				<?php endif; ?>
 
 				<tbody>
-					<?php foreach ($rows as $row) : ?>
-						<tr>
+					<?php foreach ($rows as $key => $row) : ?>
+						<tr class="elementor-repeater-item-<?php echo esc_attr( $key ); ?>">
 							<?php foreach ($row as $content) : ?>
 								<<?php echo esc_html( $content['type'] ); ?>
 								rowspan="<?php echo esc_attr( $content['row_span'] ); ?>"
-								colspan="<?php echo esc_attr( $content['col_span'] ); ?>">
+								colspan="<?php echo esc_attr( $content['col_span'] ); ?>"
+								class="elementor-repeater-item-<?php echo esc_attr( $content['id'] ); ?>" >
 
 								<?php printf( '<div class="sina-table-data">%s</div>', $content['cell_content'] ); ?>
 
