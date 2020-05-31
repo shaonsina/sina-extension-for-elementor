6b7da545dd07f0cf0e3dd32cb9d7cf97394cdfd3
shaonsina <shaon.sina@gmail.com>
2020-05-31 19:18:52 +0600

    rtl issue fixed

--- sina-extension-for-elementor.php
+++ sina-extension-for-elementor.php
@@ -3,7 +3,7 @@
  * Plugin Name: Sina Extension for Elementor
  * Plugin URI: https://sinaextra.com/sina-addon-for-elementor/
  * Description: A collection of high-quality widgets for Elementor page builder.
- * Version: 3.2.14
+ * Version: 3.2.15
  * Author: SinaExtra
  * Author URI: https://sinaextra.com/
  * Text Domain: sina-ext
@@ -17,8 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define('SINA_EXT_VERSION', '3.2.14');
-define('SINA_EXT_PREVIOUS_VERSION', '3.2.13' );
+define('SINA_EXT_VERSION', '3.2.15');
+define('SINA_EXT_PREVIOUS_VERSION', '3.2.15' );
 define('SINA_EXT_FILE', __FILE__ );
 define('SINA_EXT_SLUG', basename( SINA_EXT_FILE, '.php' ));
 define('SINA_EXT_DIR', __DIR__);
